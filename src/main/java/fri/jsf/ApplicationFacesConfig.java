fabe876c223bfd43e1ccf3c6f15ae5b283146c82
Douglas Rafael <rafael_profissional@outlook.com>
2023-01-13 15:18:44 -0300

    Home.xhtml added and some confgs changes

---
+++ src/main/java/fri/jsf/ApplicationFacesConfig.java
@@ -0,0 +1,12 @@
+package fri.jsf;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+//@FacesConfig
+//@ApplicationScoped
+public class ApplicationFacesConfig {
+	
+}
