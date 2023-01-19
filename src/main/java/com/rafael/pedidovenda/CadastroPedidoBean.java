764762e7aa99183ed73f5319803a0602a7399176
Douglas Rafael <rafael_profissional@outlook.com>
2023-01-19 20:02:38 -0300

    page CadastroPedido, your bean and yours others files were added

---
+++ src/main/java/com/rafael/pedidovenda/CadastroPedidoBean.java
@@ -0,0 +1,30 @@
+package com.rafael.pedidovenda;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+
+@SuppressWarnings("deprecation")
+@ManagedBean
+@RequestScoped
+public class CadastroPedidoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<Integer> itens;
+
+	public CadastroPedidoBean() {
+		itens = new ArrayList<>();
+		itens.add(1);
+	}
+
+	public List<Integer> getItens() {
+		return itens;
+	}
+}
+
+
---
+++ src/main/java/com/rafael/pedidovenda/PesquisaPedidosBean.java
@@ -0,0 +1,31 @@
+package com.rafael.pedidovenda;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@SuppressWarnings("deprecation")
+@ManagedBean
+@RequestScoped
+public class PesquisaPedidosBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<Integer> pedidosFiltrados;
+	
+	public PesquisaPedidosBean() {
+		pedidosFiltrados = new ArrayList<>();
+		for(int i = 0; i < 50; i++) {
+			pedidosFiltrados.add(i);
+			System.out.println(i);
+		}
+	}
+	
+	public List<Integer> getPedidosFiltrados() {
+		return pedidosFiltrados;
+	}
+}
+
